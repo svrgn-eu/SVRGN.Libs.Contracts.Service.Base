f2f4e8e7310d72c93cc74ccff3b7903e1e5c7b86
efilnukefesin <efil.threat@googlemail.com>
2023-12-30 22:53:40 +0100

    first commit

---
+++ src/SVRGN.Libs.Contracts.Service.Base/IService.cs
@@ -0,0 +1,21 @@
+﻿using SVRGN.Libs.Contracts.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SVRGN.Libs.Contracts.Service.Base
+{
+    /// <summary>
+    /// the main service interface
+    /// </summary>
+    public interface IService : IBaseObject
+    {
+        #region Properties
+
+        #endregion Properties
+
+        #region Methods
+
+        #endregion Methods
+    }
+}
